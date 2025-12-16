6dba5ca17d4174f428cd951b69683fec531c832c
sliwek-1 <msliwinski247@gmail.com>
2025-12-16 23:13:22 +0100

    Logowanie/rejestracja

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+interface FormData {
+    email: string,
+    password: string
+}
+
+export function Login() {
+
+    const initialValues = {
+        email: "",
+        password: ""
+    }
+
+    const onSubmit = (values: FormData) => {
+        console.log(values);
+    }
+
+    return (
+        <>
+            <h1>Login</h1>
+
+        </>
+    )
+}
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Register() {
+    return (
+        <>
+            <h1>Register</h1>
+        </>
+    )
+}
